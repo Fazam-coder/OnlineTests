fdf4bc679b653c06df922ed2898f1f3081725814
Amir <amirrf06@gmail.com>
2022-11-03 22:12:01 +0300

    created database sqlite

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Fazam_secret_key'
 
 
 def main():
-    db_session.global_init('db/tests.db')
     app.run()
 
 
