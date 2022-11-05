b8e611e9be90b8f1caa75a8881d567e05c232619
Amir <amirrf06@gmail.com>
2022-11-05 22:54:19 +0300

    register.html

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_login import current_user
 
+from forms.register import RegisterForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Fazam_secret_key'
 
@@ -10,9 +12,10 @@ def index():
     return render_template('base.html', title='Онлайн тесты', current_user=current_user)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    form = RegisterForm()
+    return render_template('register.html', title='Регистрация', form=form, current_user=current_user)
 
 
 def main():
